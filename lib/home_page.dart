98c8221dede27f43b1acd5e99c3357d1d10a40fb
DinOfficial <shdiinofficial19@gmail.com>
2025-10-28 03:55:12 +0300

    calculation complete

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,6 +12,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  WeightType weightType = WeightType.kg;
+  HeightType heightType = HeightType.cm;
 
 
   final kgCtrl = TextEditingController();
@@ -21,55 +23,133 @@ class _HomePageState extends State<HomePage> {
   final inchCtrl = TextEditingController();
   final mCtrl = TextEditingController();
 
+  String bmiResult = '';
 
   // 1 kg = 0.453592 lb
   double ? lbToKg(){
-    final lb = double.tryParse(lbCtrl.text);
-    if (lb == null || lb <= 0) {
+   final kgText = kgCtrl.text;
+   final lbText = lbCtrl.text;
+    if(kgText.isNotEmpty){
+      final kg = double.tryParse(kgText.trim());
+      if(kg == null || kg <= 0){
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Please enter a valid weight in KG'),
+          )
+        );
+      return null;
+      }
+      return kg;
+    }
+    else if(lbText.isNotEmpty){
+      final lb = double.tryParse(lbText.trim());
+      if(lb == null || lb <= 0){
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Please enter a valid weight in KG'),
+          )
+        );
+        return null;
+      }
+      return lb * 0.453592;
+    }else{
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Please enter a valid weight in KG'),
-        )
+          content: Text('Please enter a weight'),
+        ),
       );
+      return null;
     }
-    return lb! * 0.453592;
+
   }
 
 
   // 1 m = 100 cm
-  double ? cmToM() {
-    final cm = double.tryParse(cmCtrl.text);
-    if (cm == null || cm <= 0) {
-      ScaffoldMessenger.of(context).showSnackBar(
+
+
+
+  double? weightTypeCalculation() {
+    final cmText = cmCtrl.text;
+    final feetText = feetCtrl.text;
+    final inchText = inchCtrl.text;
+    final mText = mCtrl.text;
+
+    if(cmText.isNotEmpty){
+      final cm = double.tryParse(cmText.trim());
+      if(cm == null || cm <= 0){
+        ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text('Please enter a valid height in CM'),
-          )
-      );
+          ),
+        );
+        return null;
+      }
+      return cm / 100;
     }
-    return cm! / 100;
+    else if (feetText.isNotEmpty || inchText.isNotEmpty) {
+      final feet = double.tryParse(feetText.trim()) ?? 0.0;
+      final inch = double.tryParse(inchText.trim()) ?? 0.0;
+      if (feet <= 0 && inch <= 0) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Please enter a valid height in Feet/Inch'),
+          ),
+        );
+        return null;
+      }
+      double totalFeet = feet + (inch / 12.0);
+      // 1 feet = 0.3048 meters
+      return totalFeet * 0.3048;
+    }
+    else if (mText.isNotEmpty) {
+      final m = double.tryParse(mText.trim());
+      if (m == null || m <= 0) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Please enter a valid height in Meter'),
+          ),
+        );
+        return null;
+      }
+      return m;
+    }
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Please enter a height'),
+      ),
+    );
+    return null;
   }
 
 
-  double ? inchFeetToM(){
-    final feet = double.tryParse(feetCtrl.text);
-    final inch = double.tryParse(inchCtrl.text);
-    if (feet == null || feet <= 0 || inch == null || inch <= 0) {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Please enter a valid height in INCH/FEET'),
-          )
-      );
-    }
-    double totalFeet = feet!+(inch! / 12);
-    return totalFeet * 0.0254;
+  void calculateBMI() {
+    final weight = lbToKg();
+    final height = weightTypeCalculation();
+
+    if (weight == null || height == null) {
+      return;
     }
 
+  final bmi = weight / (height * height);
+  setState(() {
+    bmiResult = 'Your BMI is ${bmi.toStringAsFixed(2)}';
+  });
 
+  }
+
+  @override
+  void dispose() {
+    kgCtrl.dispose();
+    lbCtrl.dispose();
+    cmCtrl.dispose();
+    feetCtrl.dispose();
+    inchCtrl.dispose();
+    mCtrl.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    WeightType weightType = WeightType.kg;
-    HeightType heightType = HeightType.cm;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -107,6 +187,7 @@ class _HomePageState extends State<HomePage> {
           if (weightType == WeightType.kg) ...[
             SizedBox(height: 10),
             TextField(
+              controller:kgCtrl,
               decoration: InputDecoration(
                 hintText: 'Weight (KG)',
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
@@ -115,6 +196,7 @@ class _HomePageState extends State<HomePage> {
           ] else ...[
             SizedBox(height: 10),
             TextField(
+              controller: lbCtrl,
               decoration: InputDecoration(
                 hintText: 'Weight (LB)',
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
@@ -147,6 +229,7 @@ class _HomePageState extends State<HomePage> {
           if (heightType == HeightType.cm) ...[
             SizedBox(height: 10),
             TextField(
+              controller: cmCtrl,
               decoration: InputDecoration(
                 hintText: 'Height (CM)',
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
@@ -158,6 +241,7 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: feetCtrl,
                     decoration: InputDecoration(
                       hintText: 'Height (Feet)',
                       border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
@@ -167,6 +251,7 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(width: 10,),
                 Expanded(
                   child: TextField(
+                    controller: inchCtrl,
                     decoration: InputDecoration(
                       hintText: 'Height (INCH)',
                       border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
@@ -178,6 +263,7 @@ class _HomePageState extends State<HomePage> {
           ] else ...[
             SizedBox(height: 10),
             TextField(
+              controller: mCtrl,
               decoration: InputDecoration(
                 hintText: 'Height (MEETER)',
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
@@ -193,13 +279,13 @@ class _HomePageState extends State<HomePage> {
                 borderRadius: BorderRadius.circular(10),
               )
             ),
-              onPressed: (){},
+              onPressed: calculateBMI,
               child: Text('Show The Result', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), )
           ),
           SizedBox(height: 20,),
           Card(
             child: Center(
-              child: Text('Result'),
+              child: Text('Result : $bmiResult'),
             ),
           )
         ],
